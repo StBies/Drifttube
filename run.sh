94f0baa14c1bc4cd9df3377cc9bb6e87d0db3779
Stefan <Dinge_passieren@web.de>
2016-10-19 09:38:58 +0200

    Added commentary

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# A simple shell script, that reads all data files beginning with Ar in the data directory and executes the program for each one of those files
+# Author: Stefan, 10/19/2016.
 
 cd data
 ls Ar*.root > rawfiles.dat
