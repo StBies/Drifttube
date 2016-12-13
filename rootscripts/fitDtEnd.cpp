0de89071d45af8bb45614a456bc04b664f0a3de8
Stefan <Dinge_passieren@web.de>
2016-12-13 09:03:56 +0100

    Completed a rootscript to fit a linear function to the end of the drift time spectrum

--- rootscripts/fitDtEnd.cpp
+++ rootscripts/fitDtEnd.cpp
@@ -17,11 +17,11 @@ void fitDtEnd(TString filename)
 	TFile* f = new TFile(filename,"read");
 	f->cd("dtSpect");
 
-	TH1D* dtSpect = (TH1D*)gDirectory->Get("Drift time spectrum");
+	TH1D* dtSpect = (TH1D*)gDirectory->Get("Drifttime spectrum");
 	Int_t firstZeroBin = 0;
 
 	//find first bin with zero entries
-	for(Int_t i = 1; i <= dtSpect->GetNbinsX(); i++)
+	for(Int_t i = 5; i <= dtSpect->GetNbinsX(); i++)
 	{
 		if(dtSpect->GetBinContent(i) == 0)
 		{
@@ -29,5 +29,18 @@ void fitDtEnd(TString filename)
 			break;
 		}
 	}
+	cout << "first zero bin: " << firstZeroBin << endl;
+
+	TFitResultPtr fit = dtSpect->Fit("pol1", "S", "", (firstZeroBin*4)*0.8, (firstZeroBin*4)*1.20);
+
+	Double_t par0 = fit->Parameter(0);
+	Double_t par1 = fit->Parameter(1);
+	Double_t par0err = fit->Error(0);
+	Double_t par1err = fit->Error(1);
+
+	Double_t tmax = abs(par0/par1);
+	Double_t error = sqrt(pow(par0err,2)/pow(par1,2)+pow(par0,2)*pow(par1err,2)/pow(par1,4));
+
+	cout << "tmax = " << tmax << " +- " << error << endl;
 	//TODO complete
 }
