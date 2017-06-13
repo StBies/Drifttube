24925d329178663c3374653a835680377a2b96d6
Stefan <Dinge_passieren@web.de>
2017-06-13 13:37:53 +0200

    Implemented unit test for class DriftTimeSpectrum, from earlier pushes: new v2 branch is now main branch, fixes #34 implemented most internal data as 16 bit integers instead of 64 bit floats, fixes #25, banned ROOT framework from project, ROOT wrappers will be provided, fixes #24 C++ unique pointers used in many places, fixes #21

--- buildTest.sh
+++ buildTest.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 g++ -std=c++11 -fopenmp -I./gtest/include -L./gtest/lib src/unitTests/Event_test.cpp obj/Event.o -o EventTest.out -lgtest -lgtest_main -lpthread -fopenmp
+g++ -std=c++11 -fopenmp -I./gtest/include -L./gtest/lib src/unitTests/DriftTimeSpectrum_test.cpp obj/DriftTimeSpectrum.o obj/Event.o -o DriftTimeSpectrumTest.out -lgtest -lgtest_main -lpthread -fopenmp
 g++ -std=c++11 -fopenmp -I./gtest/include -L./gtest/lib src/unitTests/Drifttube_test.cpp obj/Drifttube.o obj/DataSet.o obj/Event.o obj/Exception.o obj/EventSizeException.o -o DrifttubeTest.out -lgtest -lgtest_main -lpthread -fopenmp
 g++ -std=c++11 -fopenmp -I./gtest/include -L./gtest/lib src/unitTests/DataSet_test.cpp obj/DataSet.o obj/Event.o obj/Exception.o obj/EventSizeException.o -o DataSet_test.out -lgtest -lgtest_main -lpthread -fopenmp
 g++ -std=c++11 -fopenmp -I./gtest/include -L./gtest/lib src/unitTests/DataProcessor_test.cpp obj/DataProcessor.o obj/Event.o obj/DataSet.o obj/Exception.o obj/EventSizeException.o -o DataProcessorTest.out -lgtest -lgtest_main -lpthread -fopenmp
---
+++ src/unitTests/DriftTimeSpectrum_test.cpp
@@ -0,0 +1,73 @@
+#include "../DriftTimeSpectrum.h"
+#include "../Event.h"
+#include <gtest/gtest.h>
+
+using namespace std;
+
+class DriftTimeSpectrumTest : public ::testing::Test
+{
+public:
+	DriftTimeSpectrumTest()
+	{
+		std::unique_ptr<std::array<uint32_t,800>> a(new std::array<uint32_t,800>);
+		std::unique_ptr<std::array<uint32_t,800>> b(new std::array<uint32_t,800>);
+		a->fill(0xFFFFFFFF);
+		b->fill(0);
+		spect1 = new DriftTimeSpectrum(move(a),25000);
+		spect2 = new DriftTimeSpectrum(move(b),0);
+
+	}
+
+	~DriftTimeSpectrumTest()
+	{
+		delete spect1;
+		delete spect2;
+	}
+
+protected:
+	DriftTimeSpectrum* spect1;
+	DriftTimeSpectrum* spect2;
+
+};
+
+TEST_F(DriftTimeSpectrumTest,TestMaxValue)
+{
+	unsigned long maxInt = 0xFFFFFFFF;
+	ASSERT_TRUE(maxInt + 1 > maxInt);
+	for(unsigned short i = 0; i < spect1->getData().size(); i++)
+	{
+		ASSERT_EQ(maxInt,(*spect1)[i]);
+	}
+}
+
+TEST_F(DriftTimeSpectrumTest,TestGetEntries)
+{
+	ASSERT_EQ(25000,spect1->getEntries());
+	ASSERT_EQ(0,spect2->getEntries());
+}
+
+TEST_F(DriftTimeSpectrumTest,TestCapacityVsEvent)
+{
+	unique_ptr<array<uint16_t,800>> a(new array<uint16_t,800>);
+	Event e(1,move(a));
+	e[0] = 0xFFFF;
+	e[0]++;
+
+	(*spect1)[0] = 0xFFFF + 1;
+
+	ASSERT_TRUE(e[0] < (*spect1)[0]);
+	ASSERT_EQ(0,e[0]);
+	ASSERT_EQ((unsigned long)0x10000,(*spect1)[0]);
+}
+
+TEST_F(DriftTimeSpectrumTest,TestOverflowAtZero)
+{
+	(*spect1)[0]++;
+	ASSERT_EQ(0,(*spect1)[0]);
+}
+
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
