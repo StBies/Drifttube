fa8af97340224758d78bce4326c28a91c3b609e7
Stefan <Dinge_passieren@web.de>
2017-09-11 10:29:41 +0200

    Corrected includes in unittests for Track and TriggerEventCollection to relative paths.

--- src/unitTests/Track_test.cpp
+++ src/unitTests/Track_test.cpp
@@ -5,5 +5,43 @@
  *      Author: bieschke
  */
 
-#include "/home/bieschke/workspace/Drifttube/src/Track.h"
+#include "../Track.h"
+#include <gtest/gtest.h>
+
+class TrackTest : public ::testing::Test
+{
+public:
+	TrackTest()
+	{
+
+	}
+
+	void SetUp()
+	{
+
+	}
+
+	void TearDown()
+	{
+
+	}
+
+	~TrackTest()
+	{
+
+	}
+
+protected:
+};
+
+TEST_F(TrackTest,Dummy)
+{
+	ASSERT_TRUE(true);
+}
+
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
 
--- src/unitTests/TriggerEventCollection_test.cpp
+++ src/unitTests/TriggerEventCollection_test.cpp
@@ -5,5 +5,43 @@
  *      Author: bieschke
  */
 
-#include "/home/bieschke/workspace/Drifttube/src/TriggerEventCollection.h"
+#include "../TriggerEventCollection.h"
+#include <gtest/gtest.h>
+
+class TriggerEventCollectionTest : public ::testing::Test
+{
+public:
+	TriggerEventCollectionTest()
+	{
+
+	}
+
+	void SetUp()
+	{
+
+	}
+
+	void TearDown()
+	{
+
+	}
+
+	~TriggerEventCollectionTest()
+	{
+
+	}
+
+protected:
+};
+
+TEST_F(TriggerEventCollectionTest,Dummy)
+{
+	ASSERT_TRUE(true);
+}
+
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
 
