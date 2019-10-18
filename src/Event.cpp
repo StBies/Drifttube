55bca4b5468ade80bcb11e5e57896a51cfd9fc8c
Stefan Bieschke <Dinge_passieren@web.de>
2019-10-18 14:32:05 +0200

    Marked the line where the drift time for an event is calculated with a TODO comment. This needs to be redesigned, see #66. Since the last point is moved to a new issue, this now fixes #62, since the possibility is now there

--- src/Event.cpp
+++ src/Event.cpp
@@ -26,6 +26,7 @@ using namespace std;
 Event::Event(unsigned int eventNumber, unique_ptr<vector<uint16_t>> data) : Data(move(data))
 {
 	m_event_number = eventNumber;
+	//TODO rework where and when to calculate this
 	m_drift_time = ADC_BINS_TO_TIME * DataProcessor::findDriftTimeBin(*this,ABSOLUTE_OFFSET_ZERO_VOLTAGE+ABSOLUTE_EVENT_THRESHOLD_VOLTAGE);
 }
 
